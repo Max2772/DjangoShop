132d9a80d31603f7b3bbb8d043bb06cca447889b
Bibikau Maxim <bib.maxim@gmail.com>
2026-02-12 20:32:59 +0300

    refactor(search): search fully done

--- goods/utils.py
+++ goods/utils.py
@@ -1,5 +1,4 @@
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
-
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector, SearchHeadline
 from goods.models import Products
 
 
@@ -9,4 +8,29 @@ def q_search(query):
 
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    result = (
+        Products.objects.annotate(
+        rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel="<span style='background-color: yellow;'>",
+            stop_sel="</span>",
+        )
+    )
+
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel="<span style='background-color: yellow;'>",
+            stop_sel="</span>",
+        )
+    )
+
+    return result

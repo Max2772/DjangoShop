6887aa12d9035108e18fca491d327b6dde9367fc
Bibikau <bib.maxim@gmail.com>
2026-02-17 20:04:23 +0300

    refactor(admin-panel): setup django admin panel

--- goods/models.py
+++ goods/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
@@ -33,8 +35,10 @@ class Products(models.Model):
     def __str__(self):
         return f'{self.name} Количество - {self.quantity}'
 
+    def get_absolute_url(self):
+        return reverse('catalog:product', kwargs={'product_slug': self.slug})
+
     def display_id(self):
         return f"{self.id:05}"
-
     def sell_price(self):
         return round(self.price * (100 - self.discount) / 100, 2) if self.discount else self.price

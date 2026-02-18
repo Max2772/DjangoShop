a161e83353aa19d14414faa4469c3caa91017076
Bibikau <bib.maxim@gmail.com>
2026-02-18 18:54:28 +0300

    fix(order-button): order button appearing in create order page

--- carts/views.py
+++ carts/views.py
@@ -64,7 +64,7 @@ def cart_change(request):
 
     referer = request.META.get('HTTP_REFERER')
     if reverse('orders:create_order') in referer:
-        context["orders"] = True
+        context["order"] = True
 
     cart_items_html = render_to_string(
         "carts/includes/included_cart.html", context, request=request)
@@ -91,7 +91,7 @@ def cart_remove(request):
 
     referer = request.META.get('HTTP_REFERER')
     if reverse('orders:create_order') in referer:
-        context["orders"] = True
+        context["order"] = True
 
     cart_items_html = render_to_string(
         "carts/includes/included_cart.html", context, request=request)
